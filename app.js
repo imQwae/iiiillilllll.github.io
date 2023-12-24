91379df589371f773232c504f5825720adfae1f0
imQwae <154329892+imQwae@users.noreply.github.com>
2023-12-24 22:05:54 +0900

    wow

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const mainPage = document.querySelector(".main");
 const playBtn = document.querySelector(".playBtn");
 const home = document.querySelector("#home");
 const playPage = document.querySelector(".playPage");
+const qflix = document.querySelector(".logo");
 
 function goPlay() {
   mainPage.classList.toggle("papop");
   playPage.classList.toggle("papop");
+
+  console.log("????");
 }
 
 function goHome() {
-  playPage.classList.add("papop");
-  mainPage.classList.remove("papop");
+  playPage.classList.toggle("papop");
+  mainPage.classList.toggle("papop");
+
+  console.log("!!!!");
 }
 
 playBtn.addEventListener("click", goPlay);
 home.addEventListener("click", goHome);
+qflix.addEventListener("click", goHome);
